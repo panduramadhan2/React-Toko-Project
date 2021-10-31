d8b2d95368d8241c7c35d5f0792faeecc6edff8a
panduramadhan2 <panduramadhan2@gmail.com>
2021-10-31 20:20:17 +0700

    React native image picker solved lanjut ke 3.17

--- src/pages/EditProfile/index.js
+++ src/pages/EditProfile/index.js
@@ -1,41 +1,162 @@
-import React, {Component} from 'react';
-import {Text, StyleSheet, View, ScrollView, Image} from 'react-native';
-import {Inputan, Pilihan, Tombol} from '../../components';
-import {dummyProfile} from '../../data';
-import {colors, fonts, responsiveHeight, responsiveWidth} from '../../utils';
+import React, { Component } from 'react';
+import { Text, StyleSheet, View, ScrollView, Image, Alert } from 'react-native';
+import { connect } from 'react-redux';
+import { getKotaList, getProvinsiList } from '../../actions/RajaOngkirAction';
+import { DefaultImage } from '../../assets';
+import { Inputan, Pilihan, Tombol } from '../../components';
+import { dummyProfile } from '../../data';
+import {
+  colors,
+  fonts,
+  getData,
+  responsiveHeight,
+  responsiveWidth,
+} from '../../utils';
+import { launchImageLibrary } from 'react-native-image-picker';
 
-export default class EditProfile extends Component {
+class EditProfile extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      dataProvinsi: [],
-      dataKota: [],
-      profile: dummyProfile,
+      uid: '',
+      nama: '',
+      email: '',
+      nohp: '',
+      alamat: '',
+      provinsi: false,
+      kota: false,
+      avatar: false,
+      avatarForDB: '',
     };
   }
 
+  componentDidMount() {
+    this.getUserData();
+    this.props.dispatch(getProvinsiList());
+  }
+
+  getUserData = () => {
+    getData('user').then(res => {
+      const data = res;
+      this.setState({
+        uid: data.uid,
+        nama: data.nama,
+        email: data.email,
+        nohp: data.nohp,
+        alamat: data.alamat,
+        kota: data.kota,
+        provinsi: data.provinsi,
+        avatar: data.avatar,
+      });
+
+      this.props.dispatch(getKotaList(data.provinsi));
+    });
+  };
+
+  ubahProvinsi = provinsi => {
+    this.setState({
+      provinsi: provinsi,
+    });
+
+    this.props.dispatch(getKotaList(provinsi));
+  };
+
+  getImage = () => {
+    launchImageLibrary(
+      {
+        quality: 1,
+        maxWidth: 500,
+        maxHeight: 500,
+        includeBase64: true,
+        selectionLimit: 1,
+        cameraType: 'front',
+      },
+      (response) => {
+        console.log('response nya: ', response);
+        if (response.didCancel || response.errorCode || response.errorMessage) {
+          Alert.alert('Error', 'Maaf sepertinya anda tidak memilih fotonya');
+        } else {
+          const source = response.assets[0].uri;
+          const fileString = `data:${response.assets[0].type};base64,${response.assets[0].base64}`;
+
+          this.setState({
+            avatar: source,
+            avatarForDB: fileString,
+          });
+        }
+      },
+    );
+  };
+
+  onSubmit = () => {
+    const { nama, alamat, nohp, provinsi, kota} =
+      this.state;
+    if (nama && nohp && alamat && provinsi && kota) {
+      //dispatch update
+      // this.props.dispatch(updateProfile(this.state))
+    } else {
+      Alert.alert("Error", "Nama No. Hp, ALamat, Kota,Provinsi harus diisi")
+    }
+  }
+
   render() {
-    const {dataKota, dataProvinsi, profile} = this.state;
+    const { nama, email, alamat, nohp, provinsi, kota, avatar } =
+      this.state;
+
+    const { getKotaResult, getProvinsiResult } = this.props;
+
     return (
       <View style={styles.pages}>
         <ScrollView showsVerticalScrollIndicator={false}>
-          <Inputan label="Nama" value={profile.nama} />
-          <Inputan label="Email" value={profile.email} />
-          <Inputan label="No. Handphone" value={profile.nomerHp} />
-          <Inputan label="Alamat" value={profile.alamat} textarea />
+          <Inputan
+            label="Nama"
+            value={nama}
+            onChangeText={nama => this.setState({ nama })}
+          />
+          <Inputan label="Email" value={email} disabled />
+          <Inputan
+            label="No. Handphone"
+            value={nohp}
+            onChangeText={nohp => this.setState({ nohp })}
+            keyboardType="number-pad"
+          />
+          <Inputan
+            label="Alamat"
+            value={alamat}
+            onChangeText={alamat => this.setState({ alamat })}
+            textarea
+          />
 
-          <Pilihan label="Provinsi" datas={dataProvinsi} />
-          <Pilihan label="Kota/Kab" datas={dataKota} />
+          <Pilihan
+            label="Provinsi"
+            datas={getProvinsiResult ? getProvinsiResult : []}
+            selectedValue={provinsi}
+            onValueChange={provinsi => this.ubahProvinsi(provinsi)}
+          />
+          <Pilihan
+            label="Kota/Kab"
+            datas={getKotaResult ? getKotaResult : []}
+            selectedValue={kota}
+            onValueChange={kota => this.setState({ kota: kota })}
+          />
 
           <View style={styles.inputFoto}>
             <Text style={styles.label}>Foto Profile :</Text>
 
             <View style={styles.wrapperUpload}>
-              <Image source={profile.avatar} style={styles.foto} />
+              <Image
+                source={avatar ? { uri: avatar } : DefaultImage}
+                style={styles.foto}
+              />
 
               <View style={styles.tombolChangePhoto}>
-                <Tombol title="Change Photo" type="text" padding={7} />
+                <Tombol
+                  title="Change Photo"
+                  type="text"
+                  padding={7}
+                  onPress={() => this.getImage()}
+                />
               </View>
             </View>
           </View>
@@ -54,6 +175,13 @@ export default class EditProfile extends Component {
     );
   }
 }
+
+const mapStateToProps = state => ({
+  getProvinsiResult: state.RajaOngkirReducer.getProvinsiResult,
+  getKotaResult: state.RajaOngkirReducer.getKotaResult,
+});
+
+export default connect(mapStateToProps, null)(EditProfile);
 
 const styles = StyleSheet.create({
   pages: {
@@ -82,7 +210,8 @@ const styles = StyleSheet.create({
   tombolChangePhoto: {
     marginLeft: 20,
     flex: 1,
-  },submit: {
-      marginVertical: 30
-  }
+  },
+  submit: {
+    marginVertical: 30,
+  },
 });
